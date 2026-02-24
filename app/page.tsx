0e58ceaa28e1188ef736f381801e9c71fef8ba1a
Sabrina Lucero <sabrina.lucero@macbookpro.home>
2026-02-24 16:27:06 +0000

    feat: show current evnvironment

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,16 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div>
+          <h1>Next.js</h1>
+          <p>
+            Entorno actual:{' '}
+            <strong>{process.env.NEXT_PUBLIC_ENV_NAME}</strong>
+          </p>
+
+
+        </div>
+
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -35,6 +45,7 @@ export default function Home() {
             center.
           </p>
         </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
