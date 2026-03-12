3840010afe63a1dfb33f9e6b8aae7d304788bb3c
Sabrina Lucero <sabrina.lucero@macbookpro.home>
2026-03-12 10:29:26 +0000

    feat(image): add side-by-side image optimization comparison

---
+++ app/image-optimization/page.tsx
@@ -0,0 +1,10 @@
+
+import ImageOptimizationComparision from "../components/ImageOptimizationComparision";
+
+export default function ImageOptimization() {
+    return (
+        <main>
+            <ImageOptimizationComparision />
+        </main>
+    )
+}
